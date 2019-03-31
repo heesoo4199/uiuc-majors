8be50d404c245008a3cbaea881935ef356e5b3f2
asherdale <asher@dales.org>
2019-03-31 18:37:17 -0500

    Update vis

--- visualization.js
+++ visualization.js
@@ -9,31 +9,40 @@ $(function() {
   //   visualize(data);
   // });
   const data = [
-    {major: "CS", startYear: 1980, endYear: 2013, startData: 2.3, endData: 10.7, startFemalePercentage: 0.4, endFemalePercentage: 0.64}
+    {department: "Enginerring", major: "CS", startYear: 1980, endYear: 2013, startData: 2.3, endData: 9.7, startFemalePercentage: 0.36, endFemalePercentage: 0.64}
   ];
+
 
   visualize(data);
 });
 
 
-var visualize = function(data) {
-  // Boilerplate:
-  var margin = { top: 50, right: 50, bottom: 150, left: 50 },
-        width = 1000 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
-    
-    var svg = d3.select("#chart")
+const visualize = function(data) {
+    const config = {
+        margin: {
+            top: 100,
+            right: 50,
+            bottom: 150,
+            left: 50
+        },
+        circleRadius: 3
+    };
+
+    config.width = 1000 - config.margin.left - config.margin.right;
+    config.height = 500 - config.margin.top - config.margin.bottom;
+
+    const svg = d3.select("#chart")
         .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .style("width", width + margin.left + margin.right)
-        .style("height", height + margin.top + margin.bottom)
+        .attr("width", config.width + config.margin.left + config.margin.right)
+        .attr("height", config.height + config.margin.top + config.margin.bottom)
+        .style("width", config.width + config.margin.left + config.margin.right)
+        .style("height", config.height + config.margin.top + config.margin.bottom)
         .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        .attr("transform", "translate(" + config.margin.left + "," + config.margin.top + ")");
 
     const yScale = d3.scaleLinear()
-        .range([height, 0])
-        .domain([0, 100]);
+        .range([config.height, 0])
+        .domain([0, 10]);
 
     // const colorScale = d3.scaleLinear()
     //     .domain([0,1])
@@ -41,40 +50,77 @@ var visualize = function(data) {
 
     svg.append('g')
         .call(d3.axisLeft(yScale));
+    
+    svg.append("g")
+        .attr("transform", "translate( " + config.width + ", 0 )")
+        .call(d3.axisRight(yScale));
 
     const xScale = d3.scaleBand()
-        .range([0, width])
+        .range([0, config.width])
         .domain(data.map((row) => row.opp))
-        .padding(0.2)
+        .padding(0.2);
 
     svg.append('g')
-        .attr('transform', `translate(0, ${height})`)
+        .attr('transform', `translate(0, ${config.height})`)
         .call(d3.axisBottom(xScale))
         .selectAll("text")
         .attr("y", 0)
         .attr("x", 9)
         .attr("dy", ".35em");
-        // .attr("transform", "rotate(45)")
-        // .style("text-anchor", "start");
 
-    svg.selectAll()
+    const slopeGroups = svg.append("g")
+        .selectAll("g")
         .data(data)
-        .enter()
-        .append('rect')
-        .attr('x', (row) => xScale(row.opp))
-        .attr('y', (row) => yScale(row.ratio))
-        .attr('height', (row) => height - yScale(row.ratio))
-        .attr('width', xScale.bandwidth());
-        // .attr('fill', function (d) {
-        //   return colorScale(d.ratio);
+        .enter().append("g")
+        .attr("class", "slope-group");
+        // .attr("id", function(d, i) {
+        //     console.log(d, i);
+        //     d.id = "group" + i;
+        //     d.values = [];
         // });
+    
+    const slopeLines = slopeGroups.append("line")
+      .attr("class", "slope-line")
+      .attr("x1", 0)
+      .attr("y1", d => yScale(d.startData))
+      .attr("x2", config.width)
+      .attr("y2", d => yScale(d.endData));
+
+    const leftSlopeCircle = slopeGroups.append("circle")
+        .attr("r", 3)
+        .attr("cy", d => yScale(d.startData))
+        .style("fill", "black");
+
+    const rightSlopeCircle = slopeGroups.append("circle")
+        .attr("r", 3)
+        .attr("cx", config.width)
+        .attr("cy", d => yScale(d.endData))
+        .style("fill", "black");
 
     svg.append("text")
-        .attr("x", (width / 2))             
-        .attr("y", 0 - (margin.top / 2))
+        .attr("x", (config.width / 2))             
+        .attr("y", 0 - (config.margin.top / 2))
         .attr("text-anchor", "middle")  
         .style("font-size", "18px")
         .style("fill", "#417cf4")
         .style("text-decoration", "underline")  
         .text("Fighting Illini Win Percentage vs Most Played Teams");
+
+    var titlesLeft = svg.append("g")
+        .attr("class", "title");
+
+    titlesLeft.append("text")
+        .attr("text-anchor", "end")
+        .attr("dx", -7.5)
+        .attr("dy", -config.margin.top / 4)
+        .text("1980");
+
+    var titlesRight = svg.append("g")
+        .attr("class", "title");
+
+    titlesRight.append("text")
+        .attr("text-anchor", "end")
+        .attr("dx", config.width + config.margin.right - 5)
+        .attr("dy", -config.margin.top / 4)
+        .text("2018");
 };
