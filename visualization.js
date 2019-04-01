2a5697b9839b42a5629629ab6742bd12f564ed9b
asherdale <asher@dales.org>
2019-03-31 22:11:36 -0500

    Add multiple slope graphs

--- visualization.js
+++ visualization.js
@@ -1,70 +1,61 @@
-
 $(function() {
-  d3.csv("majors_cleaned.csv").then(function(majors_data) {
-    d3.csv("total_enrollment.csv").then(function(enrollment_data) {
-        const majors = {};
-        const cleanData = [];
+  d3.csv("majors_slope_data.csv").then(majors => {
+    const colleges = {};
 
-        majors_data.forEach((row) => {
-            if (!(row.Major in majors)) {
-                majors[row.Major] = {startYear: parseFloat(row.Year), endYear: parseFloat(row.Year), startData: parseFloat(row.Total), endData: parseFloat(row.Total), startFemale: parseFloat(row.Female), endFemale: parseFloat(row.Female), college: row.College};
-                return;
-            }
+    majors.forEach((major) => {
+        major.startData = parseFloat(major.startData);
+        major.endData = parseFloat(major.endData);
 
-            if (row.Year < majors[row.Major].startYear) {
-                majors[row.Major].startYear = parseFloat(row.Year);
-                majors[row.Major].startData = parseFloat(row.Total);
-                majors[row.Major].startFemale = parseFloat(row.Female);
-            } else if (row.Year > majors[row.Major].endYear) {
-                majors[row.Major].endYear = parseFloat(row.Year);
-                majors[row.Major].endData = parseFloat(row.Total);
-                majors[row.Major].endFemale = parseFloat(row.Female);
-            }
-        });
+        major.startYear = parseFloat(major.startYear);
+        major.endYear = parseFloat(major.endYear);
 
-        const enrollment = {};
-        enrollment_data.forEach((row) => {
-            enrollment[row.Year] = parseInt(row.Total);
-        });
+        major.startFemale = parseFloat(major.startFemale);
+        major.endFemale = parseFloat(major.endFemale);
 
-        Object.entries(majors).forEach((pair) => {
-            const dataToAdd = pair[1];
-            dataToAdd.major = pair[0];
-
-            dataToAdd.startData = 100 * dataToAdd.startData / enrollment[dataToAdd.startYear];
-            dataToAdd.endData = 100 * dataToAdd.endData / enrollment[dataToAdd.endYear];
-            
-            if (dataToAdd.college == "Engineering") {
-                cleanData.push(dataToAdd);
-            }
-        })
-
-        console.log(cleanData);
-
-        visualize(cleanData);
+        colleges[major.college] = 1;
     });
+
+    console.log(Object.keys(colleges));
+
+    visualize(majors, "#chart-eng", "Engineering");
+    visualize(majors, "#chart-eng", "LAS");
+    visualize(majors, "#chart-edu", "Education");
+    visualize(majors, "#chart-bus", "Business");
+    visualize(majors, "#chart-media", "Media");
+    visualize(majors, "#chart-aces", "ACES");
+    visualize(majors, "#chart-avi", "Aviation");
+    visualize(majors, "#chart-art", "Fine and Applied Arts");
+    visualize(majors, "#chart-ahs", "Applied Health Sciences");
+    visualize(majors, "#chart-als", "Applied Life Sciences");
+    visualize(majors, "#chart-lir", "Labor and Industrial Relations");
+    visualize(majors, "#chart-lm", "LM");
+    visualize(majors, "#chart-isc", "iSchool");
+    visualize(majors, "#chart-law", "Law");
+    visualize(majors, "#chart-med", "Medicine");
+    visualize(majors, "#chart-ln", "LN");
+    visualize(majors, "#chart-dgs", "DGS");
+    visualize(majors, "#chart-soc", "Social Work");
   });
-//   const data = [
-//     {department: "Enginerring", major: "CS", startYear: 1980, endYear: 2013, startData: 2.3, endData: 9.7, startFemalePercentage: 0.36, endFemalePercentage: 0.64}
-//   ];
 });
 
 
-const visualize = function(data) {
+const visualize = function(majorsData, chartId, college) {
+    const data = majorsData.filter(major => major.college == college);
+
     const config = {
         margin: {
-            top: 100,
-            right: 50,
+            top: 50,
+            right: 100,
             bottom: 100,
-            left: 50
+            left: 100
         },
-        circleRadius: 3
+        circleRadius: 2.5
     };
 
-    config.width = 250 - config.margin.left - config.margin.right;
+    config.width = 350 - config.margin.left - config.margin.right;
     config.height = 500 - config.margin.top - config.margin.bottom;
 
-    const svg = d3.select("#chart")
+    const svg = d3.select(chartId)
         .append("svg")
         .attr("width", config.width + config.margin.left + config.margin.right)
         .attr("height", config.height + config.margin.top + config.margin.bottom)
@@ -97,31 +88,27 @@ const visualize = function(data) {
         .data(data)
         .enter().append("g")
         .attr("class", "slope-group");
-        // .attr("id", function(d, i) {
-        //     console.log(d, i);
-        //     d.id = "group" + i;
-        //     d.values = [];
-        // });
 
     const label = slopeGroups.append("text")
-      .attr("class", "label")
-      .style('position','absolute')
-      .attr("x", (config.width / 2))
-      .attr("y", d => yScale((d.startData+d.endData)/2)-50)
+      .attr("class", "major-label")
+      .attr("x", d => xScale((d.startYear+d.endYear)/2))
+      .attr("y", d => yScale((d.startData+d.endData)/2)-30)
+      .attr("id", d => "label" + d.major.replace(/\W+/g, ""))
       .attr("text-anchor", "middle")
-      .style("font-size", "18px")
+      .style("font-size", "13px")
       .style("visibility", "hidden")
-      .text("College of Engineering");
+      .text(d => d.major);
 
     const labelLine = slopeGroups.append("line")
-      .attr("class", "label-line")
-      .attr("x1", config.width/2)
+      .attr("class", "major-label-line")
+      .attr("x1", d => xScale((d.startYear+d.endYear)/2))
       .attr("y1", d => yScale((d.startData+d.endData)/2))
-      .attr("x2", config.width/2)
-      .attr("y2", d => yScale((d.startData+d.endData)/2)-40)
+      .attr("x2", d => xScale((d.startYear+d.endYear)/2))
+      .attr("y2", d => yScale((d.startData+d.endData)/2)-20)
+      .attr("id", d => "labelLine" + d.major.replace(/\W+/g, ""))
       .style("visibility", "hidden")
       .style("stroke", "black")
-      .style("stroke-width", 3);
+      .style("stroke-width", 2);
 
     const slopeLines = slopeGroups.append("line")
       .attr("class", "slope-line")
@@ -131,20 +118,27 @@ const visualize = function(data) {
       .attr("y2", d => yScale(d.endData))
       .attr('opacity', 0.6)
       .style("stroke", "black")
-      .style("stroke-width", 3)
-      .on('mouseenter', function () {
+      .style("stroke-width", 2)
+      .on('mouseenter', function (d) {
+        d3.selectAll(".slope-line")
+            .attr('opacity', 0.6)
+            .style("stroke-width", 2);
+
+        d3.selectAll(".major-label")
+            .style('visibility', 'hidden');
+
+        d3.selectAll(".major-label-line")
+            .style('visibility', 'hidden');
+
         d3.select(this)
             .attr('opacity', 1.0)
-            .style("stroke-width", 5)
-        label.style("visibility", "visible")
-        labelLine.style("visibility", "visible")
-      })
-      .on('mouseleave', function () {
-        d3.select(this)
-            .attr('opacity', 0.6)
-            .style("stroke-width", 3)
-        label.style("visibility", "hidden")
-        labelLine.style("visibility", "hidden")
+            .style("stroke-width", 3);
+
+        d3.select("#label" + d.major.replace(/\W+/g, ""))
+            .style('visibility', 'visible');
+
+        d3.select("#labelLine" + d.major.replace(/\W+/g, ""))
+            .style('visibility', 'visible');
       });
 
     const leftSlopeCircle = slopeGroups.append("circle")
@@ -164,23 +158,21 @@ const visualize = function(data) {
         .attr("y", 10 - (2 * config.margin.top / 3))
         .attr("text-anchor", "middle")
         .style("font-size", "18px")
-        .text("College of Engineering");
+        .text(college);
 
     const titlesLeft = svg.append("g")
-        .attr("class", "title");
-
-    titlesLeft.append("text")
+        .attr("class", "title")
+        .append("text")
         .attr("text-anchor", "end")
         .attr("dx", - 7.5)
-        .attr("dy", - config.margin.top / 4)
+        .attr("dy",  config.height + config.margin.top / 2)
         .text("1980");
 
     const titlesRight = svg.append("g")
-        .attr("class", "title");
-
-    titlesRight.append("text")
+        .attr("class", "title")
+        .append("text")
         .attr("text-anchor", "end")
-        .attr("dx", config.width + config.margin.right - 5)
-        .attr("dy", -config.margin.top / 4)
+        .attr("dx", config.width + config.margin.right / 2)
+        .attr("dy", config.height + config.margin.top / 2)
         .text("2018");
 };
